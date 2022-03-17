e6468abdb52bd89e8492a9012ec6dbed9b5b2b90
Nicollas Marinho <nicollasmarinhol@gmail.com>
2022-03-17 00:58:06 -0300

    Updates

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -1,1 +1,14 @@
-
+import '../models/user.dart';
+/*
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    nome: 'Iann',
+    email: 'bragaprestes@gmail.com',
+  ),
+  '2': const User(
+    id: '2',
+    nome: 'Alef',
+    email: 'alefmarchal@gmail.com',
+  ),
+};*/
